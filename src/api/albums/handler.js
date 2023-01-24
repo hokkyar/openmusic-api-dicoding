081e23c75add1cadb87fa40587ea956639462360
Hokky Aryasta <hokkyaryasta@gmail.com>
2023-01-24 14:52:31 +0800

    first commit

---
+++ src/api/albums/handler.js
@@ -0,0 +1,50 @@
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service
+    this._validator = validator
+  }
+
+  async postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload)
+    const albumId = await this._service.addAlbum(request.payload)
+    return h.response({
+      status: 'success',
+      message: 'Album berhasil ditambahkan',
+      data: {
+        albumId,
+      }
+    }).code(201)
+  }
+
+  async getAlbumByIdHandler(request, h) {
+    const { id } = request.params
+    const album = await this._service.getAlbumById(id)
+    return {
+      status: 'success',
+      data: {
+        album
+      }
+    }
+  }
+
+  async putAlbumByIdHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload)
+    const { id } = request.params
+    await this._service.editAlbumById(id, request.payload)
+    return {
+      status: 'success',
+      message: 'Album berhasil diperbarui',
+    }
+  }
+
+  async deleteAlbumByIdHandler(request, h) {
+    const { id } = request.params
+    await this._service.deleteAlbumById(id)
+    return {
+      status: 'success',
+      message: 'Album berhasil dihapus',
+    }
+  }
+}
+
+module.exports = AlbumsHandler
---
+++ src/api/songs/handler.js
@@ -0,0 +1,61 @@
+class SongsHandler {
+  constructor(service, validator) {
+    this._service = service
+    this._validator = validator
+  }
+
+  async postSongHandler(request, h) {
+    this._validator.validateSongPayload(request.payload)
+    const songId = await this._service.addSong(request.payload)
+    return h.response({
+      status: 'success',
+      message: 'Lagu berhasil ditambahkan',
+      data: {
+        songId,
+      }
+    }).code(201)
+  }
+
+  async getSongsHandler() {
+    const songs = await this._service.getSongs()
+    return {
+      status: 'success',
+      data: {
+        songs: songs.map(({ id, title, performer }) => ({ id, title, performer }))
+      }
+    }
+  }
+
+  async getSongByIdHandler(request, h) {
+    const { id } = request.params
+    const song = await this._service.getSongById(id)
+    return {
+      status: 'success',
+      data: {
+        song
+      }
+    }
+  }
+
+  async putSongByIdHandler(request, h) {
+    this._validator.validateSongPayload(request.payload)
+    const { id } = request.params
+    await this._service.editSongById(id, request.payload)
+    return {
+      status: 'success',
+      message: 'Lagu berhasil diperbarui',
+    }
+  }
+
+  async deleteSongByIdHandler(request, h) {
+    const { id } = request.params
+    await this._service.deleteSongById(id)
+    return {
+      status: 'success',
+      message: 'Lagu berhasil dihapus',
+    }
+  }
+
+}
+
+module.exports = SongsHandler
