12ec428d782dffffedd9bfbb9de356e5db09f49c
hokkyar <hokkyaryasta@gmail.com>
2023-03-22 11:12:41 +0700

    first commit

--- src/server.js
+++ src/server.js
@@ -46,24 +46,20 @@ const init = async () => {
     const { response } = request
     if (response instanceof Error) {
       if (response instanceof ClientError) {
-        const newResponse = h.response({
+        return h.response({
           status: 'fail',
           message: response.message
-        })
-        newResponse.code(response.statusCode)
-        return newResponse
+        }).code(response.statusCode)
       }
 
       if (!response.isServer) {
         return h.continue
       }
 
-      const newResponse = h.response({
+      return h.response({
         status: 'error',
         message: 'terjadi kegagalan pada server kami'
-      })
-      newResponse.code(500)
-      return newResponse
+      }).code(500)
     }
 
     return h.continue
